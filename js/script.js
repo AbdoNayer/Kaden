79d1e8ac5dedc3100595b8b7ae9f9e49195f752a
AbdoNayer <abdelwahabnayer20@gmail.com>
2023-10-22 00:57:33 +0300

    home

--- js/script.js
+++ js/script.js
@@ -10,69 +10,25 @@ $(window).on('load', function () {
     });
     $("body").css("overflow-y", "auto");
 
-    if ($(window).width() < 767) {
-        $("aside").addClass("in-aside");
-        document.documentElement.style.setProperty('--aside-nav-width', '100px')
-    }
-
 });
 
 $(document).ready(function () {
 
-    // // ADD IMAGE
-    $('.image-uploader').change(function (event){
-        $(this).parents('.images-upload-block').append('<div class="uploaded-block"><img src="'+ URL.createObjectURL(event.target.files[0]) +'"><button class="close"><i class="icon-trash text-white"></i></button></div>');
-    });
-    
-    // REMOVE IMAGE
-    $('.images-upload-block').on('click', '.close',function (){
-        $(this).parents('.uploaded-block').remove();
-    });
-
-    // Scroll For Section
-    $(document).on("click", ".down-in",function(e) {
-        $('html, body').animate({
-            scrollTop: $('#' + $(this).data('value')).offset().top - 100
-        }, 50);
-        e.preventDefault();
-    });
-
     // Click Open Nav In Mobile
-    $(document).on("click", ".click-nav, .overlay",function() {
+    $(document).on("click", ".click-nav, .overlay-fixed",function() {
         $(".handle").toggleClass("closed");
         $(".nav-header").toggleClass("back");
         $(".overlay-fixed").toggleClass("back");
-        $("nav li").toggleClass("animSlideIn");
         $("body").toggleClass("overflow-hidden");
-    });
-
-    // Toggle Nav
-    $(document).on("click", ".btn-fade-nav",function() {
-        $("aside").toggleClass("in-aside");
-        if ($("aside").hasClass('in-aside')) {
-            document.documentElement.style.setProperty('--aside-nav-width', '100px')
-        }else{
-            document.documentElement.style.setProperty('--aside-nav-width', '250px')
-        }
-    });
-
-    /// Show And Hide Password
-    $(document).on("click", ".icon-eye",function() {
-        $('.icon-eye').toggleClass("icon-eye-off");
-        if ($('.input-password').attr("type") === "password") {
-            $('.input-password').attr("type", "text");
-        } else {
-            $('.input-password').attr("type", "password");
-        }
     });
 
     // Scroll Fix Header
     $(window).scroll(function () {
         if ($(window).width() > 767) {
             if($(this).scrollTop() > 30){
-                $('.header-home').addClass('header-fixed')
+                $('.header').addClass('header-fixed')
             }else{
-                $('.header-home').removeClass('header-fixed')
+                $('.header').removeClass('header-fixed')
             }
         }
     });
@@ -86,6 +42,35 @@ $(document).ready(function () {
 
     // Animation Page
     AOS.init();
+
+    // Slider Skills
+    $('#sliderHome').owlCarousel({
+        loop:true,
+        margin:0,
+        center:true,
+        autoplay:true,
+        autoplayTimeout: 7000,
+        smartSpeed: 1000,
+        animateOut: 'fadeOut',
+        dragEndSpeed: 7000,
+        pagination : false,
+        nav:true,
+        navText: [
+            '<i class="icon-arr-l"></i>',
+            '<i class="icon-arr-r"></i>'
+        ],
+        responsive:{
+            0:{
+                items:1
+            },
+            900:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    });
     
 });
 
